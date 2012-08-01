37b51583f3a5d23891c68e84a27e5f7d40137759
Christos Porios <porios@yahoo.gr>
2012-08-01 10:11:45 +0300

    ApplicationController specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   private
-
   helper_method :current_user
 
   def current_user
-    @curent_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 end
