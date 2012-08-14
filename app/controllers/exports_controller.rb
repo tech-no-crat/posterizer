31b00b4ad5a85840ca5dfeebaa7e66cd07d379c0
Christos Porios <porios@yahoo.gr>
2012-08-14 10:17:56 +0300

    Exports controller w/ specs

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -3,6 +3,12 @@ class ExportsController < ApplicationController
   before_filter :require_login
 
   def create
+    if current_user.export and ((Time.now - current_user.export.created_at)/ 1.hour).floor < 2
+      render :json => {:error => "You can export your posterwall only once every 2 hours. Try again in #{((2.hours - (Time.now - current_user.export.created_at))/60).ceil}"}, :status => 429
+      return
+    end
+
+    current_user.export.destroy if current_user.export 
     export = current_user.build_export
     export.save
     ExportPosterwall.perform_async(export.id)
--- spec/controllers/exports_controller_spec.rb
+++ spec/controllers/exports_controller_spec.rb
@@ -10,7 +10,7 @@ describe ExportsController do
     end
 
     it "should create an export model for the current user, and give status 269" do
-      user = mock_model(User)
+      user = mock_model(User, :export => nil)
       export = mock_model(Export, :save => true)
       controller.stub(:current_user) { user }
       user.should_receive(:build_export) { export }
@@ -19,12 +19,21 @@ describe ExportsController do
     end
 
     it "should call perform_async on the sidekiq ExportPosterwall poster with the correct export  id" do
-      user = mock_model(User)
+      user = mock_model(User, :export => nil)
       export = mock_model(Export, :id => 42, :save => true)
       controller.stub(:current_user) { user }
       user.stub(:build_export) { export }
       ExportPosterwall.should_receive(:perform_async).with(export.id)
       post 'create'
+    end
+
+    it "should return status 429 instead of creating a new export if the user created an export request less than two hours ago" do
+      export = mock_model(Export, :created_at => Time.now - 1.hour)
+      user = mock_model(User, :export => export)
+      controller.stub(:current_user) { user }
+      user.should_not_receive(:build_export)
+      post 'create'
+      response.response_code.should == 429
     end
   end
 
